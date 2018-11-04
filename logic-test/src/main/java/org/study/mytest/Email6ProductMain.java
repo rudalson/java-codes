8e898a77e1aa5a370308ef3dcbe2d2ee6e7d7ab7
rudalson <kmpak@coupang.com>
2018-11-04 09:42:32 +0900

    added some test codes

--- logic-test/src/main/java/org/study/mytest/Email6ProductMain.java
+++ logic-test/src/main/java/org/study/mytest/Email6ProductMain.java
@@ -1,10 +1,16 @@
 package org.study.mytest;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
+import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.util.stream.Collectors.toList;
 
@@ -34,14 +40,32 @@ class Product2 {
     }
 }
 
+@AllArgsConstructor
+@Setter
+@Getter
+class VendorItem {
+    long id;
+    long productId;
+    Boolean valid;
+    Boolean logistics;
+    Boolean soldOut;
+}
+
+class VendorItemFinder {
+    static public List<VendorItem> findByProductIdIn(List<Long> productIds) {
+        return Arrays.asList(
+                new VendorItem(1L, 1L, TRUE, TRUE, FALSE), new VendorItem(2L, 1L, TRUE, TRUE, FALSE),
+                new VendorItem(3L, 2L, TRUE, FALSE, FALSE), new VendorItem(4L, 2L, TRUE, FALSE, FALSE),
+                new VendorItem(5L, 3L, TRUE, TRUE, FALSE), new VendorItem(6L, 3L, TRUE, TRUE, FALSE),
+                new VendorItem(7L, 4L, TRUE, TRUE, TRUE), new VendorItem(8L, 4L, TRUE, TRUE, TRUE),
+                new VendorItem(9L, 5L, TRUE, TRUE, FALSE), new VendorItem(10L, 5L, TRUE, TRUE, FALSE),
+                new VendorItem(11L, 6L, TRUE, TRUE, FALSE), new VendorItem(12L, 6L, TRUE, TRUE, FALSE)
+        );
+    }
+}
+
 class ProductFinder2 {
     public List<Product2> findByProductIdIn(List<Long> productIds) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        //return Optional.empty();
         return Arrays.asList(new Product2(1L, TRUE), new Product2(2L, TRUE), new Product2(3L, TRUE), new Product2(4L, TRUE), new Product2(5L, TRUE), new Product2(6L, TRUE));
     }
 }
@@ -69,24 +93,48 @@ class EmailProductInfo {
 
 public class Email6ProductMain {
 
-    static List<Long> products = Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L);
+    static List<Long> productIds = Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L);
     static ProductFinder2 productFinder = new ProductFinder2();
 
     public static void main(String[] args) {
-        List<Product2> productList = productFinder.findByProductIdIn(products);
+        List<Product2> productList = productFinder.findByProductIdIn(productIds);
 
-        if (productList.size() != products.size()) {
+        if (productList.size() != productIds.size()) {
             System.err.println("There is nonexistenct product(s)");
             return;
         }
 
-        if (productList.stream().anyMatch(pr -> !pr.getValid())) {
-            System.err.println("there is unavailable product(s)");
+        if (productList.stream().anyMatch(pr -> {
+            if (!pr.getValid()) {
+                System.err.println("there is an unavailable product : " + pr.id);
+            }
+            return !pr.getValid();
+        })) {
             return;
         }
 
-        Optional<Product2> optional = productList.stream().filter(pr -> pr.getId() == 7L).findFirst();
+        List<VendorItem> vendorItems = VendorItemFinder.findByProductIdIn(productIds);
 
+        for (Long id : productIds) {
+            List<VendorItem> vis = vendorItems.stream().filter(v -> v.getProductId()==id).collect(toList());
+
+            if (vis.parallelStream().allMatch(VendorItem::getSoldOut)) {
+                System.err.println("there is not vendor items. product :" + id);
+                return;
+            }
+
+            if (vis.parallelStream().noneMatch(VendorItem::getValid)) {
+                System.err.println("there is not available vendor item. product :" + id);
+                return;
+            }
+        }
+
+
+
+
+
+
+        Optional<Product2> optional = productList.stream().filter(pr -> pr.getId() == 7L).findFirst();
 
         long start = System.currentTimeMillis();
         ArrayList<EmailProductInfo> productInfos =
---
+++ logic-test/src/main/java/org/study/mytest/LimitString.java
@@ -0,0 +1,13 @@
+package org.study.mytest;
+
+public class LimitString {
+
+    public static void main(String[] args) {
+        String test = "";
+
+        String sub = test.substring(0, 5);
+
+        System.out.println(test);
+        System.out.println(sub.length());
+    }
+}
